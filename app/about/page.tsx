37db8c199bf8f4f4e99fb8c3020aa43d6d1e2614
Scherer Adrian <scherer.adi@gmail.com>
2023-05-24 21:36:56 +0200

    Add some links

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Image from 'next/image';
 import { name, about, bio, avatar } from 'lib/info';
+import { GitHubIcon, SquashIcon, GymIcon, StackOverflowIcon, ArrowIcon } from 'components/icons';
 
 export const metadata: Metadata = {
   title: 'About',
@@ -27,6 +28,59 @@ export default function AboutPage() {
       <p className="my-5 max-w-[580px] text-neutral-800 dark:text-neutral-200">
         {bio()}
       </p>
+      <p className="my-12 max-w-[580px] text-neutral-800 dark:text-neutral-200">
+        Here some links to places where I spend my time:
+      </p>
+      <div className="grid grid-cols-2 gap-4 max-w-[580px]">
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://github.com/schereradi"
+          className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
+        >
+          <div className="flex items-center">
+            <GitHubIcon />
+            <div className="ml-3">GitHub</div>
+          </div>
+          <ArrowIcon />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://stackoverflow.com/"
+          className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
+        >
+          <div className="flex items-center">
+            <StackOverflowIcon />
+            <div className="ml-3">Stack Overflow</div>
+          </div>
+          <ArrowIcon />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.urban-gym.ch/"
+          className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
+        >
+          <div className="flex items-center">
+            <GymIcon />
+            <div className="ml-3">Urban Gym Hochdorf</div>
+          </div>
+          <ArrowIcon />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://sqcp.ch/"
+          className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
+        >
+          <div className="flex items-center">
+            <SquashIcon />
+            <div className="ml-3">Squash Club Kriens</div>
+          </div>
+          <ArrowIcon />
+        </a>
+       </div>
     </section>
   );
 }
