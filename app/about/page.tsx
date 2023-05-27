f206ab1bca2eac78a97c100eb68c577b05747c40
Scherer Adrian <scherer.adi@gmail.com>
2023-05-27 12:43:17 +0200

    Link stack overflow to my profile

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -47,7 +47,7 @@ export default function AboutPage() {
         <a
           rel="noopener noreferrer"
           target="_blank"
-          href="https://stackoverflow.com/"
+          href="https://stackoverflow.com/users/11996556/scherera"
           className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
         >
           <div className="flex items-center">
